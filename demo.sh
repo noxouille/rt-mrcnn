633479b03e208d75066469dfbc7dff390cb1aec7
Noxouille <fp9821@gmail.com>
2019-01-17 11:44:37 +0900

    minor improvement of demo script

--- demo.sh
+++ demo.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 source activate py36
-./forever run_webcam.py
+until python run_webcam.py; do
+    echo "'run_webcam.py' crashed with exit code $?. Restarting..." >&2
+    sleep 1
+done
 source deactivate py36
